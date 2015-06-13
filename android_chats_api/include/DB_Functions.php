d30568cca8f0cc0b8f0256db2e2c381601e9a533
Shahmonil1996 <shahmonil1996@gmail.com>
2015-06-14 01:10:40 +0530

    Update DB_Functions.php

--- android_chats_api/include/DB_Functions.php
+++ android_chats_api/include/DB_Functions.php
@@ -1,7 +1,7 @@
 <?php
- 
+
 class DB_Functions {
- 
+ private $con;
     private $db;
  
     //put your code here
@@ -9,8 +9,8 @@ class DB_Functions {
     function __construct() {
         require_once 'DB_Connect.php';
         // connecting to database
-        $con->db = new DB_Connect();
-        $con->db->connect();
+        $this->db = new DB_Connect();
+        $con=$this->db->connect();
     }
  
     // destructor
@@ -22,36 +22,105 @@ class DB_Functions {
      * Storing new user
      * returns user details
      */
-    public function addChat($send, $receive,$time,$amount,$description,$chattag) {
-        
-        $result = mysql_query("INSERT INTO chats(send, receive,time,amount,description,chattag) VALUES('$send',' $receive','$time','$amount','$description','$chattag')");
+    public function storeUser($name, $email, $password, $number) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+        $result = mysqli_query($con,"INSERT INTO users(unique_id, name, email, encrypted_password, salt, created_at,number) VALUES('$uuid', '$name', '$email', '$encrypted_password', '$salt', NOW(),'$number')");
         // check for successful store
         if ($result) {
-           return true;
+            // get user details 
+            $uid = mysql_insert_id(); // last inserted id
+            $result = mysqli_query($con,"SELECT * FROM users WHERE unique_id = $uid");
+            // return user details
+            return mysql_fetch_array($result);
         } else {
             return false;
         }
     }
      /**
-     * Get Chat by Receive number
+     * Get user by Phone number
      */
-    public function checkbyReceive($receive) {
-        $result = mysql_query("SELECT * FROM chats WHERE receive LIKE '%$receive%' ") or die(mysql_error());
+    public function getUserByPhoneNumber($number) {
+        $result = mysqli_query($con,"SELECT * FROM users WHERE number='$number'") or die(mysql_error());
         // check for result 
         $no_of_rows = mysql_num_rows($result);
         if ($no_of_rows > 0) {
-           return mysql_fetch_array($result);
+		 $result = mysql_fetch_array($result);
             
+            return $result;
             }
          else {
             // user not found
             return false;
         }
     }
-    public function deletebyId($id){
-$query="DELETE FROM chats WHERE id=$id";
-mysql_query($query);
+ 
+    /**
+     * Get user by email and password
+     */
+    public function getUserByEmailAndPassword($email, $password) {
+        $result = mysqli_query($con,"SELECT * FROM users WHERE email = '$email'") or die(mysql_error());
+        // check for result 
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            $result = mysql_fetch_array($result);
+            $salt = $result['salt'];
+            $encrypted_password = $result['encrypted_password'];
+            $hash = $this->checkhashSSHA($salt, $password);
+            // check for password equality
+            if ($encrypted_password == $hash) {
+                // user authentication details are correct
+                return $result;
+            }
+        } else {
+            // user not found
+            return false;
+        }
+    }
+ 
+    /**
+     * Check user is existed or not
+     */
+    public function isUserExisted($email) {
+        $result = mysqli_query($con,"SELECT email from users WHERE email = '$email'");
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            // user existed 
+            return true;
+        } else {
+            // user not existed
+            return false;
+        }
+    }
+ 
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashSSHA($password) {
+ 
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+ 
+    /**
+     * Decrypting password
+     * @param salt, password
+     * returns hash string
+     */
+    public function checkhashSSHA($salt, $password) {
+ 
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+ 
+        return $hash;
+    }
+ 
 }
-   }
  
 ?>
